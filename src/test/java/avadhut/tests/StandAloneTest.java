6eefab1264ebb0f4d9fe8caecfe7aa4d65c6c1ba
Avadhut0123 <avadhutwakkar123@gmail.com>
2025-08-13 14:53:09 +0530

    updated standaloneTest file

--- src/test/java/avadhut/tests/StandAloneTest.java
+++ src/test/java/avadhut/tests/StandAloneTest.java
@@ -20,7 +20,7 @@ public class StandAloneTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//new comments are added 9
+		//new comments are added 10
 		
 		String productName = "ZARA COAT 3";
 		WebDriverManager.chromedriver().setup();
